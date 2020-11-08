266949a04738054a7dfbcb9a572cf2d69e7a66c7
Donny-squirtle <Donny-squirtle@github.com>
2020-11-08 12:39:59 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 const link = document.querySelector(".button-contacts");
 const popup = document.querySelector(".popup-contacts");
 const contacts_close = document.querySelector(".popup-contacts-close");
-const slider = document.querySelector(".slider");
-const sliderFirst = slider.querySelector(".slide-1");
-const sliderSecond = slider.querySelector(".slide-2");
-const sliderThird = slider.querySelector(".slide-3");
-const sliderButtonFirst = slider.querySelector(".first");
-const sliderButtonSecond = slider.querySelector(".second");
-const sliderButtonThird = slider.querySelector(".third");
+
 
 /*Eto popup*/
 link.addEventListener("click", function(evt) {
@@ -29,30 +23,39 @@ window.addEventListener("keydown", function(evt) {
     }
 });
 /*Eto slider*/
-sliderButtonFirst.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    sliderFirst.classList.add("active");
-    sliderSecond.classList.remove("active");
-    sliderThird.classList.remove("active");
-    sliderButtonFirst.classList.add("active");
-    sliderButtonSecond.classList.remove("active");
-    sliderButtonThird.classList.remove("active");
-});
-sliderButtonSecond.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    sliderSecond.classList.add("active");
-    sliderFirst.classList.remove("active");
-    sliderThird.classList.remove("active");
-    sliderButtonSecond.classList.add("active");
-    sliderButtonFirst.classList.remove("active");
-    sliderButtonThird.classList.remove("active");
-});
-sliderButtonThird.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    sliderThird.classList.add("active");
-    sliderFirst.classList.remove("active");
-    sliderSecond.classList.remove("active");
-    sliderButtonThird.classList.add("active");
-    sliderButtonFirst.classList.remove("active");
-    sliderButtonSecond.classList.remove("active");
-});
+const slider = document.querySelector(".slider");
+if (slider) {
+    const sliderFirst = slider.querySelector(".slide-1");
+    const sliderSecond = slider.querySelector(".slide-2");
+    const sliderThird = slider.querySelector(".slide-3");
+    const sliderButtonFirst = slider.querySelector(".first");
+    const sliderButtonSecond = slider.querySelector(".second");
+    const sliderButtonThird = slider.querySelector(".third");
+    sliderButtonFirst.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        sliderFirst.classList.add("active");
+        sliderSecond.classList.remove("active");
+        sliderThird.classList.remove("active");
+        sliderButtonFirst.classList.add("active");
+        sliderButtonSecond.classList.remove("active");
+        sliderButtonThird.classList.remove("active");
+    });
+    sliderButtonSecond.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        sliderSecond.classList.add("active");
+        sliderFirst.classList.remove("active");
+        sliderThird.classList.remove("active");
+        sliderButtonSecond.classList.add("active");
+        sliderButtonFirst.classList.remove("active");
+        sliderButtonThird.classList.remove("active");
+    });
+    sliderButtonThird.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        sliderThird.classList.add("active");
+        sliderFirst.classList.remove("active");
+        sliderSecond.classList.remove("active");
+        sliderButtonThird.classList.add("active");
+        sliderButtonFirst.classList.remove("active");
+        sliderButtonSecond.classList.remove("active");
+    });
+}
